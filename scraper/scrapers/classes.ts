d40558446ea6e1e215d5935bffbc0a676cc0575f
Lucas Brandstaetter <lucas@brandstaetter.tech>
2020-12-14 02:56:23 +0100

    Add basic member parsing

--- scraper/scrapers/classes.ts
+++ scraper/scrapers/classes.ts
@@ -12,23 +12,32 @@ async function parseClass(
   const classHREF = (<HTMLAnchorElement>tableRow.querySelector("a"))?.href;
   const classHREFArray = classHREF.split("#");
   const classURL = classHREFArray[0];
-  const classRef = classHREFArray[1] || "";
+  let classRef = classHREFArray[1] || `${classURL.split(".")[0]}.brief`;
+  classRef = classRef.replace(".", "\\.");
   const description = (
     tableRow.querySelector("td.description")?.textContent || ""
   ).trim();
   if (!classURL) {
     throw `Cannot find further information URL for class ${name}`;
   }
-  console.log(classURL, classRef);
-  /*const { document } = new JSDOM(
+  const { document } = new JSDOM(
     await (await Axios.get(`${baseURL}${classURL}`)).data,
-  ).window;*/
+  ).window;
+  const members: string[] = [];
+
+  for (const tableRow of document
+    .querySelector(`body > div.brief-listing > #${classRef}`)
+    ?.querySelectorAll("tr") || []) {
+    members.push(
+      (<HTMLAnchorElement>tableRow.querySelector("a"))?.innerHTML || "",
+    );
+  }
   if (name) {
     return {
       name,
       description,
       url: `${baseURL}${classHREF}`,
-      members: [],
+      members,
     };
   }
   throw "Class could not be parsed!";
--- scraper/writer/typescript/classes.ts
+++ scraper/writer/typescript/classes.ts
@@ -3,7 +3,7 @@ import fs from "fs/promises";
 import { configure, renderFile } from "eta";
 
 export async function writeClassesDefinition(classes: [factorioClass]) {
-  configure({ views: "templates", autoTrim: false });
+  configure({ views: "templates" });
   const output = (await renderFile("./typescript/classes", { classes })) || "";
   await fs.writeFile("classes.d.ts", output, "utf-8");
 }
